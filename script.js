8c679e06f3a31e05a5b9ece1dab520ae0ca783a3
shanbhat2005@gmail.com <shanbhat2005@gmail.com>
2025-02-28 14:53:40 +0530

    change

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let list = document.getElementById('list');
 btn.style.padding="25px";
 
 btn.addEventListener('click',function(){
-  let naam=prompt("enter name")
+  let naam=prompt("enter task")
   
   let insert=document.createElement('li');
   insert.textContent=`${naam}`;
